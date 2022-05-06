50438bf8fa9d38da4f70fa217ac283e0d5625766
Connor <connor2.easterbrook@live.uwe.ac.uk>
2022-05-06 13:11:32 +0100

    Initial commit

---
+++ assets/js/portfolioMain.js
@@ -0,0 +1,149 @@
+let going = false;
+let currentEl = null;
+let currentButton = null;
+
+// Language buttons
+const languageButtons = document.querySelectorAll('.lang-button');
+
+// For language large box animation
+const mainIMG = document.querySelector('#details');
+
+// For button information
+const textBox = document.querySelector('#textBox');
+
+// Close lang panel upon clicking outside
+$(document).click((event) =>
+{
+	if ($(event.target).closest('.lang-panel.shown').length === 0 && $(event.target).closest('.lang-button').length === 0 && $(event.target).closest('#textBox').length === 0)
+	{
+		undoLargeBoxAnim();
+		if (currentEl) currentEl.className = 'lang-button';
+	}
+});
+
+for (let button of languageButtons)
+{
+	// On Button Click
+	button.addEventListener('click', () =>
+	{
+		if (going) return;
+		going = true;
+
+		if (button.className.includes('active'))
+		{
+			button.className = 'lang-button';
+			undoLargeBoxAnim();
+
+			setTimeout(() =>
+			{
+				going = false;
+			}, 50);
+			return;
+		}
+
+		// Remove underline from old lang
+		if (currentEl) currentEl.className = 'lang-button';
+		button.className = 'lang-button active';
+
+		// Open Lang Button
+		currentEl = button;
+		undoLargeBoxAnim();
+
+		setTimeout(() =>
+		{
+			let panel = document.getElementById(button.getAttribute('data-panel'));
+			panel.classList.add('shown');
+			mainIMG.classList.remove('shown');
+			going = false;
+
+			// Animate all the children
+			let divs = panel.querySelectorAll('.projects>.proj');
+			let timeout = 400;
+			for (let div of divs)
+			{
+				setTimeout(() => div.classList.add('loaded'), timeout);
+				timeout += 50;
+			}
+		}, 50);
+	});
+}
+
+// Close the opening animation for the language buttons
+function undoLargeBoxAnim()
+{
+	// Hide current panel
+	let shownPanel = document.querySelector('.lang-panel.shown');
+	if (shownPanel)
+	{
+		shownPanel.classList.remove('shown');
+		shownPanel.querySelectorAll('.projects>.proj').forEach(c => c.className = 'proj');
+		textBox.classList.remove('shown');
+
+		mainIMG.classList.add('shown');
+	}
+}
+
+// Allow for buttons inside the project info to be shown only when there is a corresponding link to it in 'portfolioProjectDetails.js'
+let projectInfo = document.querySelectorAll('.proj');
+
+// After clicking on a project, display the project information
+for (let projectNumber of projectInfo)
+{
+	projectNumber.addEventListener('click', () =>
+	{
+		let projectDetails = projectText[projectNumber.getAttribute('data-project')];
+		let colour = projectNumber.parentElement.getAttribute('data-cat');
+
+		textBox.children[0].innerHTML = projectDetails.title;
+		textBox.children[1].innerHTML = projectDetails.description;
+
+		if (projectDetails.github)
+		{
+			textBox.children[4].style.display = 'block';
+			textBox.children[4].href = projectDetails.github;
+		}
+		else
+		{
+			textBox.children[4].style.display = 'none';
+		}
+
+		if (projectDetails.youtube)
+		{
+			textBox.children[5].style.display = 'block';
+			textBox.children[5].href = projectDetails.youtube;
+		}
+		else
+		{
+			textBox.children[5].style.display = 'none';
+		}
+
+		if (projectDetails.firstImage && projectDetails.secondImage)
+		{
+			textBox.children[6].style.display = 'block';
+			textBox.children[6].src = projectDetails.firstImage;
+		}
+		else
+		{
+			textBox.children[6].style.display = 'none';
+		}
+
+		if (projectDetails.firstImage && projectDetails.secondImage)
+		{
+			textBox.children[7].style.display = 'block';
+			textBox.children[7].src = projectDetails.secondImage;
+		}
+		else
+		{
+			textBox.children[7].style.display = 'none';
+		}
+
+		textBox.classList.add(colour);
+		textBox.classList.add('shown');
+		textBox.openingElement = projectNumber;
+	});
+}
+
+$('.close').click(function (e)
+{
+	textBox.classList.remove('shown');
+})
---
+++ assets/js/portfolioProjectDetails.js
@@ -0,0 +1,105 @@
+const projectText = {
+	/* EXAMPLE 
+	"Empty": 
+	{
+		title: "Example",
+		description: "This is just an example box.",
+		github: "https://github.com/GoatBandit/GoatBandit.github.io",
+	}, 
+	*/
+
+	//* GAMES
+
+	// ASGE (UNIVERSITY)
+	"Not Mario":
+	{
+		title: "Not Mario - Portfolio Game 1",
+		description: "'More Games in C++' Portfolio Game 1 - 'Not Mario'. This project served to introduce us to the ASGE game engine and developing in C++. I am happy with how the project ended up as it taught me a great deal about modern C++ programming. Specifically, 'frameworks,' 'tools and middleware,' and 'object orientation.'<br><br> Using Tiled as middleware, the first task was to establish Tiled level load support, in order to render the game world. Player-wise, movement, collision, statistics, destructibles, etc. Were all a must-have, since this game is attempting to... not be Mario... I suppose. <br><br> Below you will see the landing page, and the gameplay.",
+		github: "https://github.com/GoatBandit/-MGiC-Not-Mario",
+		firstImage: "/assets/images/portfolio/games/university/more_games_cpp_not_mario_menu.JPG",
+		secondImage: "/assets/images/portfolio/games/university/more_games_cpp_not_mario.JPG",
+	},
+	"Biohazard":
+	{
+		title: "Biohazard - Portfolio Game 2 (NetGame)",
+		description: "'More Games in C++' Portfolio Game 2 - 'Net Game'. This project served to introduce us to networking with Kissnet being used, alongside a focus on a lobby system. I am happy with how the project ended up, though there were two final versions. One without online multiplayer gameplay and one with. This was the case because the deadline came as my group and I were attempting to find the source of a memory leak. Perhaps I will come back to this at the end of the academic year and patch it up.<br><br> Once again, Tiled was used as middleware in order to render the game world in a nice way. The gameplay in the end was similar to turn-based strategy games of old, like the first XCOM game. <br><br> Below you will see the landing page, and the gameplay.",
+		github: "https://github.com/GoatBandit/-MGiC-Biohazard",
+		firstImage: "/assets/images/portfolio/games/university/more_games_cpp_biohazard.PNG",
+		secondImage: "/assets/images/portfolio/games/university/more_games_cpp_biohazard_game.JPG",
+	},
+
+	// Gameplay Programming (UNIVERSITY)
+	"Gameplay Programming":
+	{
+		title: "3D Combat Platformer",
+		description: "BSc Games Technology module - Gameplay Programming. <br><br> ",
+		// github: "https://github.com/GoatBandit/-MGiC-Not-Mario",
+		youtube: "https://youtu.be/slkqRAnRuL0",
+		firstImage: "/assets/images/portfolio/games/university/gameplay_programming_game.JPG",
+		secondImage: "/assets/images/portfolio/games/university/gameplay_programming_game2.JPG",
+	},
+
+	// Game Level Design (UNIVERSITY)
+	"Game Level Design":
+	{
+		title: "Game Level Design",
+		description: "BSc Games Technology module. <br><br> ",
+		// github: "https://github.com/GoatBandit/-MGiC-Not-Mario",
+		firstImage: "/assets/images/portfolio/games/university/game_level_design_game.JPG",
+		secondImage: "/assets/images/portfolio/games/university/game_level_design_game2.JPG",
+	},
+
+	// SOFTWARE
+	"Landmass Generator":
+	{
+		title: "Landmass Generator",
+		description: "What started out initially as a two-dimensional fantasy map generator quickly became a much larger project than I anticipated. This procedural landmass generator features a small range of options to dictate how you want your landmass developed, in either 2D or 3D. This project remains incomplete, in my opinion, due to it not being at a place that I would have liked it to be. Nonetheless, it is a piece of software in its own right that accomplishes its goal. <br><br> This project was created in Unity so its all coded in C#. It uses noise calculations to create a noise map and generate values for each 'tile'. I would like to recreate this project in the future when my skills have developed further in order to better optimise it and allow it to have more generation options. <br><br> More information can be found on the GitHub repository linked with the github image.",
+		// github: "https://github.com/GoatBandit/games/LandmassGenerator",
+		firstImage: "/assets/images/portfolio/landmass_generator.JPG",
+		secondImage: "/assets/images/portfolio/landmass_generator2.JPG",
+	},
+
+	"Plandit":
+	{
+		title: "Plandit",
+		description: "This is just an example box.",
+		// github: "https://github.com/GoatBandit/Plandit",
+	},
+
+	// FRONT END DEVELOPMENT
+	"This Website":
+	{
+		title: "This Website",
+		description: "This website was my first delve into front-end development and it took a fairly long time, with many issues, to reach a point that I am happy with. <br><br> This website uses both JQuery and JavaScript for effects and scripting due to my desire to experiment and discover which is the preferable one for me to code with in future front-end development projects. The Stylesheets are written in CSS and the rest is hand coded in HTML5. I created the website in this way for the previously-stated reason of wanting to experiment but I also felt it would be the most ideal way to begin learning front-end development. <br><br> More information can be found on the GitHub repository linked with the github image.",
+		// github: "https://github.com/GoatBandit/GoatBandit.github.io",
+	},
+
+	// EXOWEB
+	"ASOT":
+	{
+		title: "ASOT Property Services",
+		description: "",
+	},
+
+	// OTHERS
+	"Non-Euclidean Project":
+	{
+		title: "Comprehensive Creative Technologies Project",
+		description: "DEADLINE 28/04/2022<br><br>BSc Games Technology final year project. <br><br> 'CCTP' was the module that formed the self-made final year project on my course 'BSc Games Technology.' <br> My project 'Evolving Video Games with Complementary Geometries: Non-Euclidean Level Design' focused on creating sub-projects consisting of a selected game genre combined with a visual form of non-Euclidean geometry. Many sub-projects were developed, the majority of which only to a superficial level that showcases the intended combination but without needless further development. The sub-projects would be evaluated after development and compared to others to decide whether it should proceed into further development. The end result was the best sub-project being taken into full development and turned into a playable video game. <br><br> More information can be found on the GitHub Repo.",
+		// github: "https://github.com/GoatBandit/GoatBandit.github.io",
+	},
+
+	// CREDIT
+	"Credits":
+	{
+		title: "Credits",
+		description: "This section is just to give credit where credit is due. <br><br> .",
+	},
+
+	// BLANK
+	"BLANK":
+	{
+		title: "BLANK",
+		description: "This section is blank.",
+	},
+};
