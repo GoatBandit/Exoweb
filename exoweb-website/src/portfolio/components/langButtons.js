203d0e88ddd00359fe3fddea0dedcaf259a38172
Connor <connor2.easterbrook@live.uwe.ac.uk>
2022-05-17 13:32:10 +0100

    Introduction animation fix

--- exoweb-website/src/portfolio/components/langButtons.js
+++ exoweb-website/src/portfolio/components/langButtons.js
@@ -1,91 +1,118 @@
+import React, { useState, useEffect, useRef } from "react";
 import Introduction from "./introduction";
-
-let openLang = false;
-let currentEl = null;
-const languageButtons = document.querySelectorAll(".lang-button"); // Language buttons
-const mainIMG = document.querySelector('#details'); // For language large box animation
-const textBox = document.querySelector('#textBox'); // For button information
-
-// document.click((event) =>
-// {
-//     if ((event.target) === languageButtons)
-//     {
-//         console.log("Beep");
-//     }
-// });
-
-// var openLangSection = function ()
-// {
-//     var attribute = this.getAttribute("")
-// }
-
-for (var button of languageButtons)
-{
-    button.addEventListener("click", () => 
-    {
-        if (openLang)
-        {
-            return;
-        }
-        openLang = true;
-
-        if (button.className.includes('active'))
-        {
-            button.className = 'lang-button';
-            undoLargeBoxAnim();
-
-            setTimeout(() =>
-            {
-                openLang = false;
-            }, 50);
-
-            return;
-        }
-
-        // Remove underline from old lang 
-        if (currentEl) currentEl.className = 'lang-button';
-        button.className = 'lang-button active';
-
-        // Open Lang Button
-        currentEl = button;
-        undoLargeBoxAnim();
-
-        setTimeout(() =>
-        {
-            let panel = document.getElementById(button.getAttribute('data-panel'));
-            panel.classList.add('shown');
-            mainIMG.classList.remove('shown');
-            openLang = false;
-
-            // Animate all the children
-            let divs = panel.querySelectorAll('.projects>.proj');
-            let timeout = 400;
-            for (let div of divs)
-            {
-                setTimeout(() => div.classList.add('loaded'), timeout);
-                timeout += 50;
-            }
-        }, 50);
-    });
-}
-
-// Close the opening animation for the language buttons
-function undoLargeBoxAnim()
-{
-    // Hide current panel
-    let shownPanel = document.querySelector('.lang-panel.shown');
-    if (shownPanel)
-    {
-        shownPanel.classList.remove('shown');
-        shownPanel.querySelectorAll('.projects>.proj').forEach(c => c.className = 'proj');
-        textBox.classList.remove('shown');
-
-        mainIMG.classList.add('shown');
-    }
-}
 
 function LangButtons()
 {
+    let openLang = false;
+    let currentEl = null;
+    const languageButtons = document.querySelectorAll(".lang-button"); // Language buttons
+    const mainIMG = document.querySelector('#details'); // For language large box animation
+    const textBox = document.querySelector('#textBox'); // For button information
+
+    const langPanelRef = useRef();
+    const [isLangPanelOpen, setIsLangPanelOpen] = useState(false);
+
+    // Close lang panel upon clicking outside 
+    useEffect(() => 
+    {
+        function checkClickLocation(event)
+        {
+            // if ((".lang-panel-shown").contains(event.target) && (event.target).closest('.lang-button').length === 0 && (event.target).closest("#textBox").is("#textBox") === 0)
+            if (isLangPanelOpen && langPanelRef.current && !langPanelRef.current.contains(event.target))
+            {
+                undoLargeBoxAnim();
+                if (currentEl) currentEl.className = 'lang-button';
+                setIsLangPanelOpen(false);
+            }
+
+            // if (!isLangPanelOpen && )
+        }
+        document.addEventListener("mousedown", checkClickLocation);
+
+        // document.click((event) =>
+        // {
+        //     if ((event.target) === languageButtons)
+        //     {
+        //         console.log("Beep");
+        //     }
+        // });
+
+        // var openLangSection = function ()
+        // {
+        //     var attribute = this.getAttribute("")
+        // }
+
+        for (var button of languageButtons)
+        {
+            button.addEventListener("click", () => 
+            {
+                if (openLang)
+                {
+                    return;
+                }
+                openLang = true;
+
+                if (button.className.includes('active'))
+                {
+                    button.className = 'lang-button';
+                    undoLargeBoxAnim();
+
+                    setTimeout(() =>
+                    {
+                        openLang = false;
+                    }, 50);
+
+                    return;
+                }
+
+                // Remove underline from old lang
+                if (currentEl) currentEl.className = 'lang-button';
+                button.className = 'lang-button active';
+
+                // Open Lang Button
+                currentEl = button;
+                undoLargeBoxAnim();
+
+                setTimeout(() =>
+                {
+                    let panel = document.getElementById(button.getAttribute('data-panel'));
+                    panel.classList.add('shown');
+                    mainIMG.classList.remove('shown');
+                    openLang = false;
+
+                    // Animate all the children
+                    let divs = panel.querySelectorAll('.projects>.proj');
+                    let timeout = 400;
+                    for (let div of divs)
+                    {
+                        setTimeout(() => div.classList.add('loaded'), timeout);
+                        timeout += 50;
+                    }
+                }, 50);
+            });
+        }
+
+        // Close the opening animation for the language buttons
+        function undoLargeBoxAnim()
+        {
+            // Hide current panel
+            let shownPanel = document.querySelector('.lang-panel.shown');
+            if (shownPanel)
+            {
+                shownPanel.classList.remove('shown');
+                shownPanel.querySelectorAll('.projects>.proj').forEach(c => c.className = 'proj');
+                textBox.classList.remove('shown');
+
+                mainIMG.classList.add('shown');
+            }
+        }
+
+        return () =>
+        {
+            document.removeEventListener("mousedown", checkClickLocation);
+        }
+    });
+
     return (
         <>
             <div id="Header">
@@ -172,49 +199,8 @@ function LangButtons()
                     </div>
                 </div>
             </div>
-
-            <div id="boxAnim">
-            </div>
-
-            <div id="lang1" className="lang-panel">
-                <h2>Game Development</h2>
-                <h3 className="lang-desc">
-                    <span>Game Development</span> was the starting area of my programming experience and my degree is based
-                    around it.
-                    <span>Games</span> will always remain a core part of my life and I find enjoyment from
-                    <span>creating</span> all aspects of video games.
-                </h3>
-
-                <div className="projects lang1" data-cat="lang2">
-
-                    <div data-project="Not Mario" className="proj" id="notMario">
-                        <h3>Not Mario</h3>
-                        <p>ASGE (OpenGL) | C++</p>
-                        <span className="text">
-                            <h2>OOP, Memory management</h2>
-                        </span>
-                    </div>
-
-                    <div data-project="Biohazard" className="proj" id="biohazard">
-                        <h3>Biohazard</h3>
-                        <p>ASGE (OpenGL) | C++</p>
-                        <span className="text">
-                            <h2>OOP, Networking, Data serialization, Threading</h2>
-                        </span>
-                    </div>
-
-                    <div data-project="Observers" className="proj" id="observers">
-                        <h3>Observers</h3>
-                        <p>ASGE (OpenGL) | C++</p>
-                        <span className="text">
-                            <h2>OOP, Memory management, Stack-based states</h2>
-                        </span>
-                    </div>
-
-                </div>
-            </div>
         </>
-    );
+    )
 }
 
 export default LangButtons;
--- exoweb-website/src/portfolio/portfolio.js
+++ exoweb-website/src/portfolio/portfolio.js
@@ -11,6 +11,47 @@ function Portfolio()
             <div id="container">
                 <LangButtons />
 
+                <div id="boxAnim">
+                </div>
+
+                <div id="lang1" className="lang-panel">
+                    <h2>Game Development</h2>
+                    <h3 className="lang-desc">
+                        <span>Game Development</span> was the starting area of my programming experience and my degree is based
+                        around it.
+                        <span>Games</span> will always remain a core part of my life and I find enjoyment from
+                        <span>creating</span> all aspects of video games.
+                    </h3>
+
+                    <div className="projects lang1" data-cat="lang2">
+
+                        <div data-project="Not Mario" className="proj" id="notMario">
+                            <h3>Not Mario</h3>
+                            <p>ASGE (OpenGL) | C++</p>
+                            <span className="text">
+                                <h2>OOP, Memory management</h2>
+                            </span>
+                        </div>
+
+                        <div data-project="Biohazard" className="proj" id="biohazard">
+                            <h3>Biohazard</h3>
+                            <p>ASGE (OpenGL) | C++</p>
+                            <span className="text">
+                                <h2>OOP, Networking, Data serialization, Threading</h2>
+                            </span>
+                        </div>
+
+                        <div data-project="Observers" className="proj" id="observers">
+                            <h3>Observers</h3>
+                            <p>ASGE (OpenGL) | C++</p>
+                            <span className="text">
+                                <h2>OOP, Memory management, Stack-based states</h2>
+                            </span>
+                        </div>
+
+                    </div>
+                </div>
+
                 <div id="lang2" className="lang-panel">
                     <h2>Software Development</h2>
                     <h3 className="lang-desc">
@@ -24,7 +65,7 @@ function Portfolio()
                     <div className="projects lang2" data-cat="lang2">
 
                         <div data-project="Landmass Generator" className="proj"
-                            style={{ "backgroundImage" : "url(/assets/images/portfolio/software/landmass_generator.JPG)" }}>
+                            style={{ "backgroundImage": "url(/assets/images/portfolio/software/landmass_generator.JPG)" }}>
                             <h3>Landmass Generator</h3>
                             <p>Unity | C#</p>
                             <span className="text">
@@ -32,7 +73,7 @@ function Portfolio()
                             </span>
                         </div>
 
-                        <div data-project="Plandit" className="proj" style={{ "backgroundImage" : "url(/assets/images/goatLogo.png)" }}>
+                        <div data-project="Plandit" className="proj" style={{ "backgroundImage": "url(/assets/images/goatLogo.png)" }}>
                             <h3>Plandit</h3>
                             <p>(Hand Coded)</p>
                             <span className="text">
@@ -52,7 +93,7 @@ function Portfolio()
 
                     <div className="projects lang3" data-cat="lang3">
 
-                        <div data-project="Bandit" className="proj" style={{ "backgroundImage" : "url(/assets/images/portfolio/web/bandit_invoice.JPG)", "backgroundPosition" : "left" }}>
+                        <div data-project="Bandit" className="proj" style={{ "backgroundImage": "url(/assets/images/portfolio/web/bandit_invoice.JPG)", "backgroundPosition": "left" }}>
                             <h3>Bandit</h3>
                             <p>ReactJS / HTML5 / CSS / JavaScript</p>
                             <span className="text">
@@ -61,7 +102,7 @@ function Portfolio()
                         </div>
 
                         <div data-project="Exoweb" className="proj"
-                            style={{ "backgroundImage" : "url(/assets/images/portfolio/thisWebsite.jpg)" }}>
+                            style={{ "backgroundImage": "url(/assets/images/portfolio/thisWebsite.jpg)" }}>
                             <h3>Exoweb Website</h3>
                             <p>HTML5 / CSS / JavaScript / JQuery</p>
                             <span className="text">
@@ -70,7 +111,7 @@ function Portfolio()
                         </div>
 
                         <div data-project="ASOT" className="proj"
-                            style={{ "backgroundImage" : "url(/assets/images/portfolio/web/ASOT_logo.jpg)" }}>
+                            style={{ "backgroundImage": "url(/assets/images/portfolio/web/ASOT_logo.jpg)" }}>
                             <h3>ASOT Property Services Website</h3>
                             <p>Wordpress</p>
                             <span className="text">
@@ -104,7 +145,7 @@ function Portfolio()
                     <div data-project="this" className="projects others" data-cat="others">
 
                         <div data-project="Non-Euclidean Project" className="proj"
-                            style={{ "backgroundImage" : "url(/assets/images/goatLogo.png)" }}>
+                            style={{ "backgroundImage": "url(/assets/images/goatLogo.png)" }}>
                             <h3>Non-Euclidean Project</h3>
                             <p>Comprehensive Creative Technologies Project</p>
                             <span className="text">
@@ -121,7 +162,6 @@ function Portfolio()
                         This area is for the projects that I have <span>created</span> that do not fit into the given
                         <span>four</span> categories.
                     </h3>
-
                 </div>
 
                 <div id="textBox">
