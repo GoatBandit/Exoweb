54dd093304df939c3a1a7f574786984755d0e769
Connor <connor2.easterbrook@live.uwe.ac.uk>
2022-06-01 19:33:23 +0100

    Working point

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-// import Landing from "./pages/landing/landing"
-import About from "./pages/about/about"
+// import Landing from "./pages/landing/landing";
+import About from "./pages/about/about";
 import Portfolio from "./pages/portfolio/portfolio";
-
-import { Route, Routes, Navigate } from "react-router-dom"
+import { Route, Routes, Navigate } from "react-router-dom";
+import { render } from "@testing-library/react";
 
 function App()
 {
@@ -12,7 +12,7 @@ function App()
       <Routes>
         <Route exact path="/portfolio" element={<Portfolio />} />
         <Route exact path="/" element={<About />} />
-        <Route path="/*" element={<Navigate replace to="/" />} />
+        <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
     </header>
   );
